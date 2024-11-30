e586d939de74ed32c65371d02d0a44cac6f3fce2
Nick Stjern <nstjern@uw.edu>
2024-11-30 13:32:16 -0800

    update comments

--- adventurer.py
+++ adventurer.py
@@ -1,5 +1,6 @@
 import random
 
+# Class to initialize an adventurer. 
 class adventurer:
     def __init__(self): 
         self.name = ""
@@ -9,6 +10,7 @@ class adventurer:
         self.programming_pillars = []
         self.pillar_count = 0
 
+# Function to allow the user to enter a name for the hero of the game.
     def set_name(self):
         self.name = input("Enter your name: ")
         print("Your hero's name is " + self.name)
@@ -18,22 +20,26 @@ class adventurer:
     #     self.hit_points = hitpoints
     #     return self.hit_points
     
+# Function to add hit points when you find a healing potion. 
     def found_healing_potion(self):
         heal = random.randint(5, 15)
         self.hit_points += heal
         print(f"The healing potion gave you {heal} hitpoints. You now have {self.hit_points} hit points!")
 
-
+# Function to reduce hit points when you fall into a pit. 
     def pitfall(self):
         damage = random.randint(1, 20)
         self.hit_points -= damage
         print(f"You fell in a pit and lost {damage} hitpoints. You now have {self.hit_points} hit points!")
 
+# Function to add programming pillar when they are found. 
     def found_programming_pillar(self, pillar):
         if pillar: 
             self.programming_pillars.append(pillar)
             self.pillar_count += 1
 
+# Function to ouput strings for the hero's name, hit points, healing potions, vision potions, 
+# and programming pillars. 
     def __str__(self):
         formatted_string = (
             f"Your hero's name is {self.name}!\n"
@@ -45,14 +51,14 @@ class adventurer:
         return formatted_string
         
 
-hero = adventurer()
-hero.set_name()
-print(hero.hit_points)
-hero.found_healing_potion()
-hero.pitfall()
-hero.found_programming_pillar("Inheritance")
-print(hero.programming_pillars)
-print(str(hero))
+# hero = adventurer()
+# hero.set_name()
+# print(hero.hit_points)
+# hero.found_healing_potion()
+# hero.pitfall()
+# hero.found_programming_pillar("Inheritance")
+# print(hero.programming_pillars)
+# print(str(hero))
     
 
 
